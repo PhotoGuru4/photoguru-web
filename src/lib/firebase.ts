0dbfb6a249e827503a08f185da1f1a79ad1fef16
kimsa-0605 <holykimsa05@gmail.com>
2026-03-03 11:28:37 +0700

    feat(PG-17): updae firebase file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDQgp-6podXR7yElzgzisvt07yFLHlgj3s',
-  authDomain: 'photoguru-chat.firebaseapp.com',
-  projectId: 'photoguru-chat',
-  storageBucket: 'photoguru-chat.firebasestorage.app',
-  messagingSenderId: '6973831390',
-  appId: '1:6973831390:web:581da64f7176af9f2e2a4f',
-  measurementId: 'G-RN9FCWBQWP',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
